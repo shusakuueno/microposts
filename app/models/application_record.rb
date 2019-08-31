17bbab649e99eb0706feb232e70944776b533cb6
shusaku.ueno <shu.ueno@gmail.com>
2019-08-31 07:03:35 +0000

    follow, unfollow

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,10 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+  
+  def counts(user)
+    @count_microposts = user.microposts.count
+    @count_followings = user.followings.count
+    @count_followers = user.followers.count
+  end
+
 end
